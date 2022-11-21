dd5ffd3d477e9d123314a3999cab5b24705250b9
Prasanta Acharyya <acharyya.prasanta05@gmail.com>
2022-11-21 11:46:53 +0530

    syllabus page add and minor UI and UX change

---
+++ alert.js
@@ -0,0 +1,7 @@
+document.getElementById("tnp").addEventListener("click", ()=>{
+    alert("This page is under development")
+});
+
+document.getElementById("dwn").addEventListener("click", ()=>{
+    alert("This page is under development")
+});
